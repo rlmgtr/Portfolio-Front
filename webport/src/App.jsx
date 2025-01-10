1ea24b3ab3aff158ad5cf4af3eec620d59473e31
rlmgtr <rlmagtira@gmail.com>
2025-01-11 00:15:37 +0800

    Set up
    Prepared the fundamental structure

--- webport/src/App.jsx
+++ webport/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import Home from './Pages/Home'; 
+import Services from './Pages/Services'; 
+import Projects from './Pages/Projects'; 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <Router>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <header>
+          header here
+          <nav>
+            <ul>
+           <li><NavLink to="/">Home</NavLink></li> 
+           <li><NavLink to="/Services">Services</NavLink></li> 
+           <li><NavLink to="/Projects">Projects</NavLink></li> 
+            </ul>
+          </nav>
+        </header>
+
+        <div className='content'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Services" element={<Services />} />
+            <Route path="/Projects" element={<Projects />} />
+          </Routes>
+        </div>
+
+        <footer>
+          <div className='footerPart'>
+          Footer here
+          </div>
+        </footer>
+
+       
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
